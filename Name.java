7535c9739cd1874da571fe43ce6ad54ff0111227
saravana02tech <84562030+saravana02tech@users.noreply.github.com>
2021-07-28 13:39:10 +0530

    Update Name.java

--- Name.java
+++ Name.java
@@ -3,22 +3,11 @@
 class Name {
     public static void main(String[] args)
     {
-        // check if length of args array is
-        // greater than 0
-        if (args.length > 0) {
-            System.out.println(
-                "Enter a Text:");
-
- 
-
-            // iterating the args array and printing
-            // the command line arguments
-            for (String val : args)
-                System.out.println(val);
-        }
-        else
-            System.out.println("Enter a text "
-                            + "arguments found.");
+  
+            System.out.println("Enter a text ");
+        System.out.println("Enter a text ");
+        
+                            
     }
 }
  
