e23bb8405f84b8274b14b7120810fd82ce38d573
Saravana Rajamani <saravanasurya7931@gmail.com>
2021-09-23 07:17:01 +0000

    Update Name.java

--- Name.java
+++ Name.java
@@ -1,11 +1,12 @@
-
 // Program to check for command line arguments
 class Name{
     public static void main(String[] args)
     {
   
             System.out.println("shoot down");
- //         System.out.println("Enter a text ");
+       System.out.println("Enter a text ");
+              System.out.println("Enter a text ");
+
       System.out.println("messiah");
                             
     }
